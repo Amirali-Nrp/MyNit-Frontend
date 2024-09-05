15020ab3cce168c640083110e6c85eaa77d9c8b2
Amirali-Nrp <amiralinrp@gmail.com>
2024-09-05 21:43:00 +0330

    home page update

--- src/app/(Tabs)/Home/page.tsx
+++ src/app/(Tabs)/Home/page.tsx
@@ -1,42 +1,87 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import { Box, Button, Typography } from "@mui/material";
-import { RxCounterClockwiseClock } from "react-icons/rx";
 import { GrPlan } from "react-icons/gr";
-import {useRouter} from "next/navigation"
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { RxCounterClockwiseClock } from "react-icons/rx";
 
 const Home = () => {
   const nav = useRouter();
-  return <Box className="w-full md:w-1/2 m-24 md:p-4 flex flex-col md:flex-row gap-4 items-center justify-center">
-    <Button
-      onClick={() => {nav.replace("/PreCourseSelect")}}
-      variant="contained"
-      className=" items-center rounded-xl border-2 border-[#002] bg-white flex flex-col gap-4 w-full md:w-1/2 p-10 h-full"
-      sx={{direction: "rtl", "&:hover": {
-        backgroundColor: '#eee',
-      },
-      border: "2px solid black",
-      backgroundColor: "#fff"
-    }}
-    >
-      <GrPlan color="#000" size={50} />
-      <Typography sx={{fontFamily: "Vazirmatn", color: "black"}} variant="h6">مشاهده دروس قابل اخذ ترم آینده</Typography>
-    </Button>
-    <Button
-      onClick={() => {nav.replace("/Chart")}}
-      variant="contained"
-      className=" items-center rounded-xl border-2 border-[#002] bg-white flex flex-col gap-4 w-full md:w-1/2 p-10  h-full"
-      sx={{direction: "rtl", "&:hover": {
-        backgroundColor: '#eee',
-      },
-      border: "2px solid black",
-      backgroundColor: "#fff"
-    }}
-    >
-      <RxCounterClockwiseClock color="#000" size={50}/>
-      <Typography sx={{fontFamily: "Vazirmatn", color: "black"}} variant="h6">مشاهده ترم های اخیر</Typography>
-    </Button>
-  </Box>
+  return (
+    <Box className="m-24 flex w-full flex-col items-center justify-center gap-4 md:w-fit md:flex-row md:p-4">
+      <Button
+        onClick={() => {
+          nav.replace("/PreCourseSelect");
+        }}
+        variant="contained"
+        className=" flex h-full w-full flex-col items-center gap-4 rounded-xl border-2 border-[#002] bg-white p-10 md:w-1/2"
+        sx={{
+          direction: "rtl",
+          "&:hover": {
+            backgroundColor: "#eee",
+          },
+          border: "2px solid black",
+          backgroundColor: "#fff",
+        }}
+      >
+        <GrPlan color="#000" size={50} />
+        <Typography
+          sx={{ fontFamily: "Vazirmatn", color: "black" }}
+          variant="h6"
+        >
+          مشاهده دروس قابل اخذ ترم آینده
+        </Typography>
+      </Button>
+      <Button
+        onClick={() => {
+          nav.replace("/Chart");
+        }}
+        variant="contained"
+        className=" flex h-full w-full flex-col items-center gap-4 rounded-xl border-2 border-[#002] bg-white p-10  md:w-1/2"
+        sx={{
+          direction: "rtl",
+          "&:hover": {
+            backgroundColor: "#eee",
+          },
+          border: "2px solid black",
+          backgroundColor: "#fff",
+        }}
+      >
+        <RxCounterClockwiseClock color="#000" size={50} />
+        <Typography
+          sx={{ fontFamily: "Vazirmatn", color: "black" }}
+          variant="h6"
+        >
+          مشاهده ترم های اخیر
+        </Typography>
+      </Button>
+      <Button
+        onClick={() => {
+          nav.replace("/AddCourses");
+        }}
+        variant="contained"
+        className=" flex h-full w-full flex-col items-center gap-4 rounded-xl border-2 border-[#002] bg-white p-10  md:w-1/2"
+        sx={{
+          direction: "rtl",
+          "&:hover": {
+            backgroundColor: "#eee",
+          },
+          border: "2px solid black",
+          backgroundColor: "#fff",
+        }}
+      >
+        <IoIosAddCircleOutline color="#000" size={50} />
+        <Typography
+          sx={{ fontFamily: "Vazirmatn", color: "black" }}
+          variant="h6"
+        >
+          افزودن اطلاعات
+        </Typography>
+      </Button>
+    </Box>
+  );
 };
 
 export default Home;
